3d60aa2ebff67399df569c56ffa3afe8236281e9
Daniel Ivanov <sun.is.still.high@gmail.com>
2025-12-18 19:08:17 +0300

    Add debug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import debug from 'debug';
 import fs from 'fs/promises';
 import path from 'path';
 import * as cheerio from 'cheerio';
+
+const log = debug('page-loader');
 
 const formatName = (str) => str
   .replace(/[^a-zA-Z0-9]/g, '-')
@@ -31,9 +34,18 @@ const generateAssetFileName = (url, assetPath) => {
   return `${formatName(fullPath)}.html`;
 };
 
-const downloadAsset = (assetUrl, assetPath) => axios
-  .get(assetUrl, { responseType: 'arraybuffer' })
-  .then((response) => fs.writeFile(assetPath, response.data));
+const downloadAsset = (assetUrl, assetPath) => {
+  log('downloading asset: %s', assetUrl);
+  return axios
+    .get(assetUrl, { responseType: 'arraybuffer' })
+    .then((response) => {
+      log('asset downloaded, size: %d bytes', response.data.length);
+      return fs.writeFile(assetPath, response.data);
+    })
+    .then(() => {
+      log('asset saved to: %s', assetPath);
+    });
+};
 
 const resourceMapping = [
   { tag: 'img', attr: 'src' },
@@ -42,17 +54,23 @@ const resourceMapping = [
 ];
 
 const pageLoader = (url, outputDir = process.cwd()) => {
+  log('loading page: %s', url);
+  log('output directory: %s', outputDir);
+
   const fileName = generateFileName(url);
   const filePath = path.resolve(outputDir, fileName);
   const assetsDirName = generateAssetsDirName(url);
   const assetsDirPath = path.resolve(outputDir, assetsDirName);
   const baseUrl = new URL(url);
 
+  log('output file: %s', filePath);
+
   let $;
   const assets = [];
 
   return axios.get(url)
     .then((response) => {
+      log('page loaded, status: %d', response.status);
       $ = cheerio.load(response.data);
 
       resourceMapping.forEach(({ tag, attr }) => {
@@ -61,10 +79,15 @@ const pageLoader = (url, outputDir = process.cwd()) => {
           if (!attrValue) return;
 
           const assetUrl = new URL(attrValue, url);
-          if (assetUrl.hostname !== baseUrl.hostname) return;
+          if (assetUrl.hostname !== baseUrl.hostname) {
+            log('skipping external resource: %s', assetUrl.href);
+            return;
+          }
 
           const assetFileName = generateAssetFileName(url, attrValue);
           const newAttrValue = `${assetsDirName}/${assetFileName}`;
+
+          log('found local resource: %s -> %s', attrValue, newAttrValue);
 
           $(elem).attr(attr, newAttrValue);
           assets.push({
@@ -74,15 +97,24 @@ const pageLoader = (url, outputDir = process.cwd()) => {
         });
       });
 
+      log('found %d local resources', assets.length);
+
       if (assets.length === 0) {
         return fs.writeFile(filePath, $.html());
       }
 
+      log('creating assets directory: %s', assetsDirPath);
       return fs.mkdir(assetsDirPath, { recursive: true })
         .then(() => Promise.all(assets.map((asset) => downloadAsset(asset.url, asset.filePath))))
-        .then(() => fs.writeFile(filePath, $.html()));
+        .then(() => {
+          log('all assets downloaded');
+          return fs.writeFile(filePath, $.html());
+        });
     })
-    .then(() => filePath);
+    .then(() => {
+      log('page saved to: %s', filePath);
+      return filePath;
+    });
 };
 
 export default pageLoader;
